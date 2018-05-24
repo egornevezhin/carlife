869dc6aa49c9a98f88f20a3206febdbdeb0e26ec
Egor <egor_nevezhin@mail.ru>
2018-05-24 16:24:40 +0300

    Обновил описания для классов.

--- carlife/services/admin.py
+++ carlife/services/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 from .models import Service, Master
 # Register your models here.
 
-admin.site.register(Service)
-admin.site.register(Master)
+
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'work')
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cost')
+
+
+admin.site.register(Master, MasterAdmin)
+admin.site.register(Service, ServiceAdmin)
--- carlife/services/models.py
+++ carlife/services/models.py
@@ -2,15 +2,28 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 # Create your models here.
+
+
 class Master(models.Model):
-    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Мастер'
+        verbose_name_plural = 'Мастера'
+
+    name = models.CharField(max_length=30, verbose_name='ФИО')
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
-    work = models.CharField(max_length=20)
+    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True, verbose_name='Телефон')
+    work = models.CharField(max_length=20, verbose_name='Область работы')
+
 
 class Service(models.Model):
-    name = models.CharField(max_length=20)
-    cost = models.FloatField()
-    person = models.ForeignKey(Master, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+
+    name = models.CharField(max_length=20, verbose_name='Название')
+    cost = models.FloatField(verbose_name='Цена')
+    person = models.ForeignKey(Master, on_delete=models.CASCADE, verbose_name='Работник')
 
